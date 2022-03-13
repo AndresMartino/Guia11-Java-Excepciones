ac6fc1bdc3de2e1adf4c0111ec00b0dc29b52e9c
Andres Martino Camacho <andrelomartino@gmail.com>
2022-03-13 15:38:59 -0300

    3

--- Ej3Guia11/src/Ej1/Main.java
+++ Ej3Guia11/src/Ej1/Main.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ej1;
 
-/**
- *
- * @author Andrelo
- */
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner read = new Scanner(System.in, "ISO-8859-1").useDelimiter("\n").useLocale(Locale.US);
+        DivisionNumero division1 = new DivisionNumero();
+        System.out.println("Ingrese primer numero");
+        String num1 = read.next();
+        System.out.println("Ingrese segundo numero");
+        String num2 = read.next();
+        try {
+            int n1 = Integer.parseInt(num1);
+            int n2 = Integer.parseInt(num2);
+
+            System.out.println("La division de "+n1+"/"+n2+" es :"+division1.division(n1, n2));
+
+        } catch (NumberFormatException e) {
+            System.err.println("ERROR! Incompatibilidad de tipos");
+        } catch (ArithmeticException e) {
+            System.err.println("ERROR! Division por cero!");
+        }
+
     }
-    
+
 }
+/*Defina una clase llamada DivisionNumero. En el método main utilice un Scanner para
+leer dos números en forma de cadena. A continuación, utilice el método parseInt() de la
+clase Integer, para convertir las cadenas al tipo int y guardarlas en dos variables de tipo
+int. Por ultimo realizar una división con los dos numeros y mostrar el resultado.*/
