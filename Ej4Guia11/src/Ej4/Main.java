6ded1df669c1f8e6d059ee97130ff8a0fe7e0b1a
Andres Camacho <andrelomartino@gmail.com>
2022-03-14 09:41:53 -0300

    ej4

---
+++ Ej4Guia11/src/Ej4/Main.java
@@ -0,0 +1,78 @@
+package Ej4;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in, "ISO-8859-1").useDelimiter("\n").useLocale(Locale.US);
+        Operaciones operacion = new Operaciones();
+        int opc = 0;
+        String n1, n2;
+
+        do {
+            try {
+
+                System.out.println("----MENU----");
+                System.out.println("1-Sumar");
+                System.out.println("2-Restar");
+                System.out.println("3-Multiplicar");
+                System.out.println("4-Dividir");
+                System.out.println("5-Salir");
+                opc = read.nextInt();
+
+                switch (opc) {
+                    case 1:
+                        System.out.println("Ingrese primero numero");
+                        n1 = read.next();
+                        System.out.println("Ingrese segundo numero");
+                        n2 = read.next();
+                        operacion.cargarNumeros(Integer.parseInt(n1), Integer.parseInt(n2));
+                        System.out.println("La suma es: " + operacion.suma());
+                        break;
+                    case 2:
+                        System.out.println("Ingrese primero numero");
+                        n1 = read.next();
+                        System.out.println("Ingrese segundo numero");
+                        n2 = read.next();
+                        operacion.cargarNumeros(Integer.parseInt(n1), Integer.parseInt(n2));
+                        System.out.println("La resta es: " + operacion.resta());
+                        break;
+                    case 3:
+                        System.out.println("Ingrese primero numero");
+                        n1 = read.next();
+                        System.out.println("Ingrese segundo numero");
+                        n2 = read.next();
+                        operacion.cargarNumeros(Integer.parseInt(n1), Integer.parseInt(n2));
+                        System.out.println("La multiplicacion es: " + operacion.producto());
+                        break;
+                    case 4:
+                        System.out.println("Ingrese primero numero");
+                        n1 = read.next();
+                        System.out.println("Ingrese segundo numero");
+                        n2 = read.next();
+                        operacion.cargarNumeros(Integer.parseInt(n1), Integer.parseInt(n2));
+                        System.out.println("La division es: " + operacion.cociente());
+                        break;
+                    case 5:
+                        System.out.println("Saliendo");
+                        break;
+                    default:
+                        System.out.println("opcion incorrecta!");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Error de Tipos");
+
+            } catch (NumberFormatException e) {
+                System.err.println("Error de tipos2");
+            } catch (ArithmeticException e) {
+                System.err.println("ERROR division por cero");
+            }
+        } while (opc != 5);
+
+    }
+
+}
